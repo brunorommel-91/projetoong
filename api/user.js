b6a0f3be9152369ae9a36cfb056915af41b9bdfd
Roberto <robertoruizcai@gmail.com>
2020-09-02 18:33:33 -0300

    ajustes mongo

--- api/user.js
+++ api/user.js
@@ -7,56 +7,87 @@ module.exports = app => {
         const salt = bcrypt.genSaltSync(10)
         return bcrypt.hashSync(password, salt)
     }
-    const save = async (req, res) =>{
-        const user = { ...req.body}
-        if(req.params.id) user.id = req.params.id
+    const save = async (req, res) => {
+        const user = req.body
+        const { name, ongname, endrua, endbairro, endcidade, endestado, email, password, cofirmPassword } = user
 
-        try{
-            existsOrError(user.name, 'Nome nao Informado')
-            existsOrError(user.ongname, 'Nome da ONG nao Informado')
-            existsOrError(user.endrua, 'Rua nao Informada')
-            existsOrError(user.endbairro, 'Bairro nao Informado')
-            existsOrError(user.endcidade, 'Cidade nao Informada')
-            existsOrError(user.endestado, 'Estado nao Informado')
-            existsOrError(user.email, 'E-mail não informado')
-            existsOrError(user.password, 'Senha não informada')
-            existsOrError(user.confirmPassword, 'Confirmação de Senha Invalida')
-            equalsOrError(user.password, user.confirmPassword, 'Senha não conferem')
-
-            const userFromDB = await app.db('users')
-                .where({ email: user.email }).first()
-                if(!user.id) {
-                    notExistsOrError(userFromDB, 'Usuario ja cadastrado')
-                }
-        }catch(msg) {
-            return res.status(400).send(msg)
-        }
-
-        user.password = encryptPassword(user.password)
-        delete user.confirmPassword
-
-        if(user.id) {
-            app.db('users')
-                 .update(user)
-                 .where({ id: user.id})
-                 .whereNull('deletedAt')
-                 .then(_ => res.status(204). send())
-                 .catch(err => res.status(500).send(err))
-        }else{
-            app.db('users')
-                .insert(user)
-                .then(_ => res.status(204).send())
-                .catch(err => res.status(500).send(err))
-        }
+        app.db.connect(app.uri, function(err, client) {
+        const collection = client.db(app.dbName).collection("users");
+    
+        collection.find({"email": email}).toArray(function (err, result) {
+            if (result.length > 0) {
+                res.status(400).send('Já existe um usuário com esse nome');
+            } else {
+                collection.insertOne(user, {w: 1}, function(err, records){
+                    if (err) {
+                        res.status(400).send(err);
+                    } else {
+                        res.status(200).send('Usuário inserido com sucesso');
+                    }
+                })
+            }
+        })
+        })
     }
+    // const save = async (req, res) =>{
+    //     const user = { ...req.body}
+    //     if(req.params.id) user.id = req.params.id
+
+    //     try{
+    //         existsOrError(user.name, 'Nome nao Informado')
+    //         existsOrError(user.ongname, 'Nome da ONG nao Informado')
+    //         existsOrError(user.endrua, 'Rua nao Informada')
+    //         existsOrError(user.endbairro, 'Bairro nao Informado')
+    //         existsOrError(user.endcidade, 'Cidade nao Informada')
+    //         existsOrError(user.endestado, 'Estado nao Informado')
+    //         existsOrError(user.email, 'E-mail não informado')
+    //         existsOrError(user.password, 'Senha não informada')
+    //         existsOrError(user.confirmPassword, 'Confirmação de Senha Invalida')
+    //         equalsOrError(user.password, user.confirmPassword, 'Senha não conferem')
+
+    //         const userFromDB = await app.db('users')
+    //             .where({ email: user.email }).first()
+    //             if(!user.id) {
+    //                 notExistsOrError(userFromDB, 'Usuario ja cadastrado')
+    //             }
+    //     }catch(msg) {
+    //         return res.status(400).send(msg)
+    //     }
+
+    //     user.password = encryptPassword(user.password)
+    //     delete user.confirmPassword
+
+    //     if(user.id) {
+    //         app.db('users')
+    //              .update(user)
+    //              .where({ id: user.id})
+    //              .whereNull('deletedAt')
+    //              .then(_ => res.status(204). send())
+    //              .catch(err => res.status(500).send(err))
+    //     }else{
+    //         app.db('users')
+    //             .insert(user)
+    //             .then(_ => res.status(204).send())
+    //             .catch(err => res.status(500).send(err))
+    //     }
+    // }
 
     const get = (req, res) => {
-        app.db('users')
-            .select('id', 'name', 'ongname', 'endrua', 'endbairro', 'endcidade', 'endestado', 'email', 'admin')
-            .whereNull('deletedAt')
-            .then(users => res.json(users))
-            .catch(err => res.status(500).send(err))
+        app.db.connect(app.uri, function(err, client) {
+            const collection = client.db(app.dbName).collection("users");
+        
+            collection.find().toArray(function (err, result) {
+                res.status(200).send(result);
+            })
+        });
     }
+    // const get = (req, res) => {
+    //     app.db('users')
+    //         .select('id', 'name', 'ongname', 'endrua', 'endbairro', 'endcidade', 'endestado', 'email', 'admin')
+    //         .whereNull('deletedAt')
+    //         .then(users => res.json(users))
+    //         .catch(err => res.status(500).send(err))
+    // }
     const getById = (req, res) => {
         app.db('users')
             .select('id', 'name', 'ongname', 'endrua', 'endbairro', 'endcidade', 'endestado', 'email', 'admin')
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const app =  require('express')()
-const db = require('./config/db')
+// const db = require('./config/db')
 const consign = require ('consign')
 const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient
+var uri = "mongodb+srv://ongbr:4ivAF8Ho9dqKD0er@cluster0.giejo.mongodb.net/postgresql?retryWrites=true&w=majority"
 
-
-app.db = db
-mongoose.connect('mongodb+srv://ongbr:4ivAF8Ho9dqKD0er@cluster0.giejo.mongodb.net/postgresql?retryWrites=true&w=majority', 
-{useNewUrlParser: true , useUnifiedTopology: true}).then(() => {
-    console.log('Mongodb Conectado....')
-})
+app.db = MongoClient
+app.uri = uri
+app.dbName = "queroajudar"
+// mongoose.connect('mongodb+srv://ongbr:4ivAF8Ho9dqKD0er@cluster0.giejo.mongodb.net/postgresql?retryWrites=true&w=majority', 
+// {useNewUrlParser: true , useUnifiedTopology: true}).then(() => {
+//     console.log('Mongodb Conectado....')
+// })
 
 consign()
     .then('./config/middlewares.js')
@@ -21,3 +24,68 @@ consign()
 app.listen(3000, () =>{
     console.log('Backend Executando....')
 })
+
+// var express = require('express')
+// const bodyParser = require('body-parser')
+// const mongo = require('mongodb').MongoClient
+
+// const port = 3000
+// var app = express()
+
+// app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
+
+// var MongoClient = require('mongodb').MongoClient
+// var uri = "mongodb+srv://ongbr:4ivAF8Ho9dqKD0er@cluster0.giejo.mongodb.net/postgresql?retryWrites=true&w=majority"
+
+// const getDefault = (request, response) => {
+//     response.status(200).send('API no ar');
+// }
+
+// const userInsert = (request, response) => {
+//     const { userName, password } = request.body
+  
+//     MongoClient.connect(uri, function(err, client) {
+//       const collection = client.db("login").collection("user");
+  
+//       collection.find({"userName": userName}).toArray(function (err, result) {
+//         if (result.length > 0) {
+//           response.status(400).send('Já existe um usuário com esse nome');
+//         } else {
+//           var document = {userName:userName, password:password};
+//           collection.insertOne(document, {w: 1}, function(err, records){
+//             if (err) {
+//               response.status(400).send(err);
+//             } else {
+//               response.status(200).send('Usuário inserido com sucesso');
+//             }
+//           });
+//         }
+//       })
+//     });
+//   }
+
+//   const userGet = (request, response) => {
+//     MongoClient.connect(uri, function(err, client) {
+//       const collection = client.db("login").collection("user");
+  
+//       collection.find().toArray(function (err, result) {
+//         console.log(result)
+//         response.status(200).send(result);
+//       })
+//     });
+// }
+
+// app.get('/', getDefault)
+// app.post('/user', userInsert)
+// app.get('/user', userGet)
+
+// app.listen(port, () => {
+//     console.log(`App running on port ${port}.`)
+//   })
+  
+// module.exports = app
